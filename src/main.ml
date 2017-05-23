96c953590bcd5801c18c91911df7773f5ec89539
Nephe <hetier.guillaume@gmail.com>
2017-05-23 11:59:36 -0400

    Improve result function insertion

--- src/main.ml
+++ src/main.ml
@@ -19,11 +19,34 @@ let outputFile (filename: string option) (f : C.file) : unit =
   C.dumpFile (!C.printerForMaincil) c output_file f;
   if filename <> None then close_out c
 
-(* --- Dirty insertion of _ltl2ba_result, just to be sure the function is not deleted *)
+(* --- Dirty insertion of _ltl2ba_result, just to be sure the function is not deleted.
+   Does not insert if there is no return in main
+*)
+class insert_result_visitor (result_call) = object(self)
+  inherit C.nopCilVisitor
+  method vstmt s =
+    match s.C.skind with
+    | C.Return _ -> let action s =
+                      C.Block (C.mkBlock [C.mkStmtOneInstr result_call; s]) |> C.mkStmt
+      in
+      C.ChangeDoChildrenPost (s, action)
+    | _ -> C.DoChildren
+end
+
 let insert_end_main rcal g =
+  let rec getLast = function
+    | [] -> assert false
+    | x::[] -> x
+    | x::q -> getLast q
+  in
   match g with
   | C.GFun (f, _) when f.C.svar.C.vname = "main" ->
-    f.C.sbody.C.bstmts <- f.C.sbody.C.bstmts @ [C.mkStmtOneInstr rcal]
+    let vis = new insert_result_visitor rcal in
+    ignore(C.visitCilFunction vis f);
+    let last = getLast (f.sbody.bstmts) in
+    (match  last.skind with
+    | C.Return _ -> ()
+    | _ -> f.sbody.bstmts <- f.sbody.bstmts @ [C.mkStmtOneInstr rcal])
   | _ -> ()
 
 let add_result f =
