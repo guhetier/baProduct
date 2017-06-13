790baec1b64a7744d311df6d404825f8a4e4db79
Nephe <hetier.guillaume@gmail.com>
2017-06-13 17:13:21 -0400

    Create an ocaml graph automaton from json

--- src/automaton.ml
+++ src/automaton.ml
@@ -5,6 +5,106 @@ module H = Hashtbl
 
 open J.Util
 
+
+type
+module Node = struct
+  type t = {id: int; final: bool}
+  let compare = Pervasives.compare
+  let equal v1 v2 = v1.id = v2.id
+  let hash v = Hashtbl.hash v.id
+end
+
+
+module Edge = struct
+  type t = {pos: string list; neg: string list}
+  let compare = Pervasives.compare
+  let default = {pos = []; neg = []}
+end
+
+
+module A = Graph.Persistent.Digraph.ConcreteBidirectionalLabeled(Node)(Edge)
+
+
+type automaton = {
+  nb_states: int;
+  nb_sym: int;
+  symbols: string list;
+  init_state: Node.t;
+  graph: A.t;
+}
+
+
+let json_to_edge (json: J.json): Edge.t * Node.t =
+  {
+    Edge.pos = List.map to_string (json |> member "pos" |> to_list);
+    Edge.neg = List.map to_string (json |> member "neg" |> to_list);
+  },
+  {
+    Node.id = json |> member "dest" |> to_int;
+    Node.final = false; (* Node are identified by their id only, so final
+                           if not important here : all nodes have already been
+                           inserted with correct values when adding the edges *)
+  }
+
+
+let json_to_state (json: J.json): Node.t =
+  {
+    Node.id = json |> member "label" |> to_int;
+    Node.final = json |> member "final" |> to_bool;
+  }
+
+
+let json_to_state_trans (json: J.json): A.edge list =
+  let vid = json |> member "label" |> to_int in
+  let edges = List.map json_to_edge (json |> member "trans" |> to_list) in
+  let v = {
+    Node.id = vid;
+    Node.final = json |> member "final" |> to_bool;
+  } in
+  List.map (fun (e,d) -> v, e, d) edges
+
+
+exception NodeFound of Node.t
+
+
+let from_json (json: J.json) : automaton =
+  let graph = A.empty in
+
+  (* Add states to the graph *)
+  let states = List.map json_to_state (json |> member "states" |> to_list) in
+  let graph = List.fold_left A.add_vertex graph states in
+
+  (* Add edges to the graph *)
+  let edges = List.map json_to_state_trans
+      (json |> member "states" |> to_list) in
+  let edges = List.flatten edges in
+  let graph = List.fold_left A.add_edge_e graph edges in
+
+  (* Get the first node back *)
+  let init_id = json |> member "init_label" |> to_int in
+  let init_state = try
+      A.iter_vertex
+        (fun v -> if v.Node.id = init_id then raise (NodeFound v)) graph;
+      raise Not_found;
+    with | NodeFound v -> v
+         | Not_found -> E.s (E.error "Initial node not found")
+  in
+  {
+    nb_states = json |> member "nb_state" |> to_int;
+    nb_sym = json |> member "nb_sym" |> to_int;
+    symbols = List.map to_string (json |> member "symbols" |> to_list);
+    init_state = init_state;
+    graph = graph;
+  }
+
+
+let from_file (filename: string) : automaton =
+  let json = try J.from_file filename
+    with _ -> E.s (E.error "Couldn't parse the automaton %s" filename)
+  in from_json json
+
+
+(*
 type label = int
 
 type transition = {
@@ -21,6 +121,8 @@ and state = {
 
 type automaton = {
   nb_states: int;
+  nb_sym: int;
+  symbols: string list;
   init_state: state;
   states: (label, state) H.t;
 }
@@ -58,11 +160,10 @@ let from_json (json: J.json) : automaton =
   in
   {
     nb_states = nb_states;
+    nb_sym = json |> member "nb_sym" |> to_int;
+    symbols = List.map to_string (json |> member "symbols" |> to_list);
     init_state = init;
     states = states
   }
+*)
 
-let from_file (filename: string) : automaton =
-  let json = try J.from_file filename
-    with _ -> E.s (E.error "Couldn't parse the file %s" filename)
-  in from_json json
